ef072ce46210875f7472e21f8cb9739ac775582c
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-07-08 09:42:29 +0300

    Ecommerce lab(balance,category,discount added.)

---
+++ src/com/company/StaticConstants.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import com.company.balance.Balance;
+import com.company.category.Category;
+import com.company.category.Electronic;
+import com.company.discount.Discount;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaticConstants {
+
+    public static final List<Customer> CUSTOMER_LIST = new ArrayList<>();
+    public static final List<Category> CATEGORY_LIST = new ArrayList<>();
+    public static final List<Product> PRODUCT_LIST = new ArrayList<>();
+    public static final List<Balance> CUSTOMER_BALANCE_LIST = new ArrayList<>();
+    public static final List<Balance> GIFT_CARD_BALANCE_LIST = new ArrayList<>();
+    public static final List<Discount> DISCOUNT_LIST = new ArrayList<>();
+
+
+}
